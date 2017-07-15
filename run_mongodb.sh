56dc0d136b9fe7a7e615a561ba74d22f7be6112f
Annika Berger <annika.c.berger@gmail.com>
2017-07-15 15:27:19 +0200

    allow quotes in pathname

--- run_mongodb.sh
+++ run_mongodb.sh
@@ -2,4 +2,4 @@
 if [ -z "$(docker images | grep 'cessor/mongodb')" ]; then
     docker build -t cessor/mongodb -f mongodb.Dockerfile .
 fi
-docker run --name mongodb -d -v $(pwd)/data:/data/db cessor/mongodb
+docker run --name mongodb -d -v "$(pwd)"/data:/data/db cessor/mongodb
