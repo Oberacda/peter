ac6e09b98b53f33347b63dac1ffd198f193f2d0c
Cloud-init-user <debian@api-usability.localdomain>
2018-06-15 11:11:03 +0000

    Changed port configuration.  Committer: David Oberacker <david.oberacker@student.kit.edu>

--- run.sh
+++ run.sh
@@ -2,4 +2,4 @@
 if [ -z "$(docker images | grep 'cessor/peter')" ]; then
     docker build -t cessor/peter -f Dockerfile .
 fi
-docker run --name peter -d -p 5000:5000 --link mongodb:mongodb -v "$(pwd)"/src:/var/peter cessor/peter
+docker run --name peter -d -p 80:80 --link mongodb:mongodb -v "$(pwd)"/src:/var/peter cessor/peter
